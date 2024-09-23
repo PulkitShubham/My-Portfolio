7b2d03dc45da6aedc8b07ec62e44d6a30a173c8a
Pulkit Shubham <pulkitshubham11@gmail.com>
2024-09-23 20:28:59 +0530

    modified Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
     <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
       <div className="container mx-auto">
         {/*nav inner*/}
-        <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[600px] mx-auto px-8 flex justify-between items-center text-2xl text-white/50">
+        <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[500px] mx-auto px-8 flex justify-between items-center text-2xl text-white/50">
           <Link
             to="home"
             activeclass="active"
@@ -26,15 +26,6 @@ const Nav = () => {
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BiUser />
-          </Link>
-          <Link
-            to="contact"
-            activeclass="active"
-            smooth={true}
-            spy={true}
-            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
-          >
-            <BiClipboard />
           </Link>
           <Link
             to="projects"
