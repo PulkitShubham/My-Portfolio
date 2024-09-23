c9a658005782a6a26bc0050210a23cc17afbc43e
Pulkit Shubham <pulkitshubham11@gmail.com>
2024-09-23 22:00:20 +0530

    Added avtar

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,16 +5,19 @@ import { motion } from "framer-motion";
 import {} from "../variants";
 const Banner = () => {
   return (
-    <section className="section" id="home">
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+      id="home"
+    >
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-8">
           {/*text*/}
-          <div>
-            <h1>
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <h1 className="text-[55px] font-bold leading-[0.8] lg:text-[90px]">
               PULKIT <span>SHUBHAM</span>
             </h1>
             <div
-              className="mb-6 text-[36px] lg:text-[60px] font-secondary
+              className="mb-6 text-[28px] lg:text-[45px] font-secondary
             font-semibold uppercase leading-[1]"
             >
               <span className="text-white mr-4">I am a</span>
@@ -32,12 +35,34 @@ const Banner = () => {
                 wrapper="span"
                 repeat={Infinity}
               />
-              <p></p>
+            </div>
+            <p className="mb-8 max-w-lg mx-auto lg:mx-0">
+              Currently immersed in the exciting world of Computer Science and
+              Engineering. My journey is fueled by a genuine love for technology
+              and an eagerness to overcome challenges.
+            </p>
+            <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+            {/*socials*/}
+            <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+              <a href="#">
+                <FaLinkedin />
+              </a>
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaTwitter />
+              </a>
             </div>
           </div>
           {/*text*/}
           <div>
-            <img src="../avtar.png" />
+            <img src="../avtar.png" alt="Avatar" />
           </div>
         </div>
       </div>
