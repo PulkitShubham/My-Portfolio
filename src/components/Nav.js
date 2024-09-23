28a752cd15a947d70be457a006de0e17064d7051
Pulkit Shubham <pulkitshubham11@gmail.com>
2024-09-23 19:30:56 +0530

    Updated Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { Link } from "react-scroll";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="fixed bottom-2 lg:bottom-8">
+      <div className="container mx-auto">
+        {/*nav inner*/}
+        <div>
+          <Link>
+            <BiHomeAlt />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
