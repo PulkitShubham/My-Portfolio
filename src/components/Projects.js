fda7ecedd355a0576a2d088b6bf492888fe046e2
Pulkit Shubham <pulkitshubham11@gmail.com>
2024-09-28 12:33:42 +0530

    Added EmaliJS

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import {
   Container,
   Wrapper,
@@ -13,92 +12,53 @@ import {
 import ProjectCard from "./ProjectCards.jsx";
 import { projects } from "../components/constants";
 
-const Projects = ({ openModal, setOpenModal }) => {
+const Projects = () => {
   const [toggle, setToggle] = useState("all");
+
   return (
     <Container id="projects">
       <Wrapper>
         <Title>Projects</Title>
         <Desc>
-          I have worked on a wide range of projects. From web apps to android
+          I have worked on a wide range of projects. From web apps to Android
           apps. Here are some of my projects.
         </Desc>
         <ToggleButtonGroup>
-          {toggle === "all" ? (
-            <ToggleButton active value="all" onClick={() => setToggle("all")}>
-              All
-            </ToggleButton>
-          ) : (
-            <ToggleButton value="all" onClick={() => setToggle("all")}>
-              All
-            </ToggleButton>
-          )}
+          <ToggleButton
+            active={toggle === "all"}
+            onClick={() => setToggle("all")}
+          >
+            All
+          </ToggleButton>
           <Divider />
-          {toggle === "web app" ? (
-            <ToggleButton
-              active
-              value="web app"
-              onClick={() => setToggle("web app")}
-            >
-              WEB APP'S
-            </ToggleButton>
-          ) : (
-            <ToggleButton value="web app" onClick={() => setToggle("web app")}>
-              WEB APP'S
-            </ToggleButton>
-          )}
+          <ToggleButton
+            active={toggle === "web app"}
+            onClick={() => setToggle("web app")}
+          >
+            WEB APP'S
+          </ToggleButton>
           <Divider />
-          {toggle === "android app" ? (
-            <ToggleButton
-              active
-              value="android app"
-              onClick={() => setToggle("android app")}
-            >
-              ANDROID APP'S
-            </ToggleButton>
-          ) : (
-            <ToggleButton
-              value="android app"
-              onClick={() => setToggle("android app")}
-            >
-              ANDROID APP'S
-            </ToggleButton>
-          )}
+          <ToggleButton
+            active={toggle === "android app"}
+            onClick={() => setToggle("android app")}
+          >
+            ANDROID APP'S
+          </ToggleButton>
           <Divider />
-          {toggle === "machine learning" ? (
-            <ToggleButton
-              active
-              value="machine learning"
-              onClick={() => setToggle("machine learning")}
-            >
-              MACHINE LEARNING
-            </ToggleButton>
-          ) : (
-            <ToggleButton
-              value="machine learning"
-              onClick={() => setToggle("machine learning")}
-            >
-              MACHINE LEARNING
-            </ToggleButton>
-          )}
+          <ToggleButton
+            active={toggle === "machine learning"}
+            onClick={() => setToggle("machine learning")}
+          >
+            MACHINE LEARNING
+          </ToggleButton>
         </ToggleButtonGroup>
         <CardContainer>
-          {toggle === "all" &&
-            projects.map((project) => (
-              <ProjectCard
-                project={project}
-                openModal={openModal}
-                setOpenModal={setOpenModal}
-              />
-            ))}
           {projects
-            .filter((item) => item.category == toggle)
+            .filter(
+              (project) => toggle === "all" || project.category === toggle
+            )
             .map((project) => (
-              <ProjectCard
-                project={project}
-                openModal={openModal}
-                setOpenModal={setOpenModal}
-              />
+              <ProjectCard key={project.id} project={project} />
             ))}
         </CardContainer>
       </Wrapper>
